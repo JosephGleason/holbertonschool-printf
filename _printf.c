a886740a15236c0ecc7c416b398da52e05ff9228
josephg <9719@holbertonstudents.com>
2025-03-25 16:43:33 -0400

    Handle the following conversion specifiers:

--- _printf.c
+++ _printf.c
@@ -2,6 +2,50 @@
 #include <stdarg.h>
 #include <unistd.h>
 /**
+ * print_number - Prints an integer to stdout
+ * @n: The integer to print
+ *
+ * Return: Number of characters printed
+ */
+int print_number(int n)
+{
+	char buffer[12]; /* enough to hold -2147483648\0 */
+	int i = 0, j, count = 0;
+	unsigned int num;
+
+	if (n == 0)
+	{
+		write(1, "0", 1);
+		return (1);
+	}
+
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		count++;
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+
+	while (num > 0)
+	{
+		buffer[i++] = (num % 10) + '0';
+		num = num / 10;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		write(1, &buffer[j], 1);
+		count++;
+	}
+
+	return (count);
+}
+
+/**
  * _printf - Produces output according to a format.
  * @format: The format string.
  *
@@ -70,6 +114,11 @@ int _printf(const char *format, ...)
 				}
 
 			}
+			else if (format[i] == 'd' || format[i] == 'i')
+			{
+				int num = va_arg(args, int);
+				word_count += print_number(num);
+			}
 
 			else
 			{
@@ -86,7 +135,7 @@ int _printf(const char *format, ...)
 			i++;
 		}
 	}
-			va_end(args);
-			return (word_count);
-		
+	va_end(args);
+	return (word_count);
+
 }
