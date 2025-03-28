a4d7b2a2b6cff0d717e3412b703c675d02f6e668
HISvessel <kevinsanchezdabest@yahoo.com>
2025-03-28 14:01:46 -0400

    last fix of all documents

---
+++ my_printf.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * my_printf - this prints in format way
+ * @format: the format string
+ * Return: number of characters printed
+ */
+int my_printf(const format, ...)
+{
+	int count = 0
+	int i, j;
+	va_list = args;
+
+	// list of functions
+
+	Funclist List[] = {
+		{"c", print_char},
+		{"s",}
+	}
+	if(format == NULL || )
+	{
+		return (-1);
+	}
+	for (format = 0; format != NULL, format++)
+	{
+		if (format[i] != '%')
+		{
+			write(1, &format(i), 1)
+			break;
+		}
+		else if
+			for (; format(i) = '%', format(i)++)
+			funcl
+				break;
+		else
+			break;
+	}
+}
